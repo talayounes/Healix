e6fdf13f24bf0ae13d56600b1b04ec877767159b
alaakmhmd <alaamohamma68@gmail.com>
2025-02-10 15:46:10 +0200

    Create App.jsx

---
+++ Front-end/emergency/EmergencyButton/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HoldButton from "./components/HoldButton";
+
+function App() {
+  return (
+    <div>
+      <HoldButton />
+    </div>
+  );
+}
+
+export default App;
